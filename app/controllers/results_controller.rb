41db198f76353dc3aeda4bacd85d07f61059bdee
Matt Graham <kickthebox@gmail.com>
2023-05-26 13:34:33 -0400

    with code for results

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -1,6 +1,6 @@
 class ResultsController < ApplicationController
     def index        
-        if params[:code] = "2023"
+        if params[:code] == 'graham'
             @results = Prediction.all
         else
             redirect_to root_path
